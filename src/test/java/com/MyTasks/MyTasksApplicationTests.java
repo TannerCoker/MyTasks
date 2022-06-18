1e16338dd8b44aed7dcfcdea5472212ae72ce743
Tanner Coker <tbcoker13@gmail.com>
2022-06-18 01:01:57 -0500

    retrieved files that I somehow lost when setting up the back-end folder

---
+++ src/test/java/com/MyTasks/MyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
