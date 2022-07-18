4555d889c1064bd26ad1e59a460155ad9572f461
Tanner Coker <tbcoker13@gmail.com>
2022-07-18 13:30:39 -0500

    added the initial validators for customerDTO

--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/dto/CustomerDTO.java
+++ Backend/MyTasks/src/main/java/com/tcoker/MyTasks/dto/CustomerDTO.java
@@ -10,9 +10,18 @@ public class CustomerDTO {
 	@NotNull //add missing email message
 	@Pattern(regexp = "[A-Za-z0-9._]+@[A-Za-z]{2,}\\.[A-Za-z][A-Za-z.]+") //add wrong pattern message
 	private String emailId;
+	@NotNull //add missing name message
+	@Pattern(regexp = "([A-Za-z])+(\\s[A-Za-z]+)*") //name pattern wrong
 	private String name;
+	@NotNull //missing password
+	@Pattern(regexp = ".*[A-Z]+.*") //add invalid messages
+	@Pattern(regexp = ".*[a-z]+.*")
+	@Pattern(regexp = ".*[0-9]+.*")
+	@Pattern(regexp = ".*[^A-Za-z0-9].*")
 	private String password;
 	private String newPassword;
+	@NotNull
+	@Pattern(regexp = "[0-9]{10}") //bad number message
 	private String phoneNumber;
 	private List<TaskDTO> myTasks;
 	
