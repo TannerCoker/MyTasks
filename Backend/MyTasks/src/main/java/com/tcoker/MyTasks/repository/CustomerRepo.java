4289fceed797443e7590756045234739b5bbec3b
Tanner Coker <tbcoker13@gmail.com>
2022-07-19 11:39:22 -0500

    comitting since the previously named 'customer' files are removed since I renamed them.

--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/repository/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tcoker.MyTasks.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tcoker.MyTasks.entity.Customer;
-
-public interface CustomerRepo extends CrudRepository<Customer,Integer> {
-
-}
--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/CustomerService.java
+++
@@ -1,5 +0,0 @@
-package com.tcoker.MyTasks.service;
-
-public class CustomerService {
-
-}
