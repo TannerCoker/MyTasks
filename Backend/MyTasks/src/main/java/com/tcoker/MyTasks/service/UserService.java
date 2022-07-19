6c62867ebd2dc9f56712f269d2da6f6ae39c2f8e
Tanner Coker <tbcoker13@gmail.com>
2022-07-19 12:40:36 -0500

    implemented the verifyLogin function in the user service class and changed the return type to UserDTO instead of boolean

--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/UserService.java
+++ Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/UserService.java
@@ -4,7 +4,7 @@ import com.tcoker.MyTasks.dto.UserDTO;
 
 public interface UserService {
 	
-	Boolean verifyLogin(String emailId, String password) throws Exception;
+	UserDTO verifyLogin(String emailId, String password) throws Exception;
 	String addUser(UserDTO dto) throws Exception;
 	UserDTO getUser(String emailId) throws Exception;
 	Boolean deleteUser(String emailId) throws Exception;
--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/UserServiceImpl.java
+++ Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package com.tcoker.MyTasks.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tcoker.MyTasks.dto.TaskDTO;
 import com.tcoker.MyTasks.dto.UserDTO;
+import com.tcoker.MyTasks.entity.User;
+import com.tcoker.MyTasks.exceptions.TaskException;
 import com.tcoker.MyTasks.repository.UserRepo;
+import com.tcoker.MyTasks.utility.HashingUtility;
 
 @Service
 @Transactional
@@ -16,9 +23,37 @@ public class UserServiceImpl implements UserService{
 	private UserRepo repo;
 
 	@Override
-	public Boolean verifyLogin(String emailId, String password) throws Exception{
-		// TODO Auto-generated method stub
-		return null;
+	public UserDTO verifyLogin(String emailId, String password) throws Exception{
+		User user = repo.findByEmail(emailId);
+		if(user == null) {
+			throw new TaskException("Service.Invalid_Email");
+		}
+//		if(!user.getPassword().equals(HashingUtility.getHashValue(password))) {
+//			throw new TaskException("Service.Invalid_Login");
+//		}
+		//not going to use the hashing utility tool yet for testing purposes
+		if(!password.equals(user.getPassword())) {
+			throw new TaskException("Service.Invalid_Login");
+		}
+		
+		UserDTO dto = new UserDTO();
+		dto.setEmailId(user.getEmail());
+		dto.setName(user.getName());
+		dto.setPhoneNumber(user.getPhoneNumber());
+		if(!user.getMyTasks().isEmpty()) {
+			List<TaskDTO> taskDTOs;
+			taskDTOs = user.getMyTasks().stream().map(t -> {
+				TaskDTO tdto = new TaskDTO();
+				tdto.setTaskId(t.getTaskId());
+				tdto.setTaskGroup(t.getTaskGroup());
+				tdto.setTaskDescription(t.getTaskDescription());
+				tdto.setDate(t.getDate());
+				return tdto;
+			}).collect(Collectors.toList());
+			dto.setMyTasks(taskDTOs);
+		}
+		
+		return dto;
 	}
 
 	@Override
