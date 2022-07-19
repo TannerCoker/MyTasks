47488f6b5b72c70d39ffdbf4b51a6105b0edd9f8
Tanner Coker <tbcoker13@gmail.com>
2022-07-19 11:38:36 -0500

    Made some empty functions/placeholders for the UserAPI class.

--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/api/UserAPI.java
+++ Backend/MyTasks/src/main/java/com/tcoker/MyTasks/api/UserAPI.java
@@ -1,8 +1,31 @@
 package com.tcoker.MyTasks.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tcoker.MyTasks.dto.UserDTO;
 
 @RestController
 public class UserAPI {
-
+	
+	public ResponseEntity<Boolean> validateLogin(String email, String password){
+		
+		
+	}
+	
+	public ResponseEntity<UserDTO> getUser(String email) {
+		
+	}
+	
+	public ResponseEntity<UserDTO> addUser(UserDTO newCustomer) {
+		
+	}
+	
+	public ResponseEntity<Boolean> deleteUser(UserDTO user) {
+		
+	}
+	
+	public ResponseEntity<UserDTO> modifyUser(UserDTO user) {
+		
+	}
 }
