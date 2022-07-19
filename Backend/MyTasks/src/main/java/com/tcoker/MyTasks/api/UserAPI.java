cd55ba0241594b2e91b4a63c50bca42fe0c90774
Tanner Coker <tbcoker13@gmail.com>
2022-07-19 12:04:20 -0500

    Added annotation to the api functions and to the class. Also setup an autowire for the userService class

--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/api/UserAPI.java
+++ Backend/MyTasks/src/main/java/com/tcoker/MyTasks/api/UserAPI.java
@@ -1,31 +1,54 @@
 package com.tcoker.MyTasks.api;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tcoker.MyTasks.dto.UserDTO;
+import com.tcoker.MyTasks.service.UserService;
 
 @RestController
+@RequestMapping(value = "user")
+@CrossOrigin
+@Validated
 public class UserAPI {
 	
-	public ResponseEntity<Boolean> validateLogin(String email, String password){
-		
+	@Autowired
+	private UserService serv;
+	
+	@GetMapping("/validate/{email}/{password}")
+	public ResponseEntity<Boolean> validateLogin(@PathVariable String email,@PathVariable String password){
 		
 	}
 	
-	public ResponseEntity<UserDTO> getUser(String email) {
+	@GetMapping("/retrieve/{email}")
+	public ResponseEntity<UserDTO> getUser(@PathVariable String email) {
 		
 	}
 	
-	public ResponseEntity<UserDTO> addUser(UserDTO newCustomer) {
+	@PostMapping("/add")
+	public ResponseEntity<Boolean> addUser(@Valid @RequestBody UserDTO newCustomer) {
 		
 	}
 	
-	public ResponseEntity<Boolean> deleteUser(UserDTO user) {
+	@DeleteMapping("/remove/{email}")
+	public ResponseEntity<Boolean> deleteUser(@PathVariable String email) {
 		
 	}
 	
-	public ResponseEntity<UserDTO> modifyUser(UserDTO user) {
+	@PutMapping("/update")
+	public ResponseEntity<Boolean> modifyUser(@Valid @RequestBody UserDTO user) {
 		
 	}
 }
