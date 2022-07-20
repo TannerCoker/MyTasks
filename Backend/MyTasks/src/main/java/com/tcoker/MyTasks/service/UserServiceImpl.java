01150ff8a2501ce40b1b04f7956633a85bf89b9a
Tanner Coker <tbcoker13@gmail.com>
2022-07-19 20:44:42 -0500

    implemented deleteUser function in userService

--- Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/UserServiceImpl.java
+++ Backend/MyTasks/src/main/java/com/tcoker/MyTasks/service/UserServiceImpl.java
@@ -27,14 +27,14 @@ public class UserServiceImpl implements UserService{
 	public UserDTO verifyLogin(String emailId, String password) throws Exception{
 		User user = repo.findByEmail(emailId);
 		if(user == null) {
-			throw new TaskException("Service.Invalid_Email");
+			throw new TaskException("Service.USER_NOT_FOUND");
 		}
 //		if(!user.getPassword().equals(HashingUtility.getHashValue(password))) {
 //			throw new TaskException("Service.Invalid_Login");
 //		}
 		//not going to use the hashing utility tool yet for testing purposes
 		if(!password.equals(user.getPassword())) {
-			throw new TaskException("Service.Invalid_Login");
+			throw new TaskException("Service.INVALID_LOGIN");
 		}
 		
 		UserDTO dto = new UserDTO();
@@ -85,14 +85,37 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public UserDTO getUser(String emailId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		User user = repo.findByEmail(emailId);
+		if(user == null) {
+			throw new TaskException("Service.USER_NOT_FOUND");
+		}
+		UserDTO dto = new UserDTO();
+		dto.setEmailId(user.getEmail());
+		dto.setName(user.getName());
+		dto.setPhoneNumber(user.getPhoneNumber());
+		if(!user.getMyTasks().isEmpty()) {
+			List<TaskDTO> taskDTOs;
+			taskDTOs = user.getMyTasks().stream().map(t -> {
+				TaskDTO tdto = new TaskDTO();
+				tdto.setTaskId(t.getTaskId());
+				tdto.setTaskGroup(t.getTaskGroup());
+				tdto.setTaskDescription(t.getTaskDescription());
+				tdto.setDate(t.getDate());
+				return tdto;
+			}).collect(Collectors.toList());
+			dto.setMyTasks(taskDTOs);
+		}
+		return dto;
 	}
 
 	@Override
 	public Boolean deleteUser(String emailId) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		User user = repo.findByEmail(emailId);
+		if(user == null) {
+			throw new TaskException("Service.USER_NOT_FOUND");
+		}
+		repo.delete(user);
+		return true;
 	}
 
 	@Override
